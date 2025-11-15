39190b810515aaacb151225feefc40af693605e7
PrudhviNVM <nagulaprudhvi@gmail.com>
2025-11-15 18:09:20 +0530

    MFAPI

--- app/backend/routers/MFapi/mf_api.py
+++ app/backend/routers/MFapi/mf_api.py
@@ -1,7 +1,7 @@
 import requests
 from fastapi import APIRouter, HTTPException
 
-API_URL = "https://api.mfapi.in/mf?limit=100&offset=0" 
+API_URL = "https://api.mfapi.in/mf?limit=&offset=0" 
 
 HISTORY_URL = "GET https://api.mfapi.in/mf/125497?startDate=2023-01-01&endDate=2023-12-31"
 data = requests.get(HISTORY_URL).json()
